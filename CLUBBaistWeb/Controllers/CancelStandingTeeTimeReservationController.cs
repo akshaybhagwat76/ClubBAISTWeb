b9a6c1b141b588b1bccc44d59816b989e159eba2
akshaybhagwat76 <akshaybhagwat76@gmail.com>
2020-02-29 16:28:33 +0530

    completed CancelStandingTeeTimeReservation

---
+++ CLUBBaistWeb/Controllers/CancelStandingTeeTimeReservationController.cs
@@ -0,0 +1,23 @@
+﻿using CLUBBaistWeb.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CLUBBaistWeb.Controllers
+{
+    public class CancelStandingTeeTimeReservationController : Controller
+    {
+        // GET: CancelStandingTeeTimeReservation
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Cancel()
+        {
+            var Cancelled = new ReservationRepository().CancelStandingReservation(Convert.ToInt32(Session["MemberNumber"].ToString()));
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
